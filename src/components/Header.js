6e74b973e3a17ffa22ff72f9ca344728e2237f41
Vbeni <benivillafana@gmail.com>
2023-07-25 15:52:33 -0700

    introducting styled-components library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
 
-function Header() {
+const HeaderContainer = styled.header`
+
+`;
+
+const ThemeToggle = styled.button`
+  
+`;
+
+const Header = () => {
+  // Use useState to manage the theme state (dark/light mode)
+  const [theme, setTheme] = useState('light');
+
+  // Function to toggle the theme
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div>Header</div>
-  )
-}
+    <HeaderContainer>
+      <h1>Personal Dashboard</h1>
+      <ThemeToggle onClick={toggleTheme}>Toggle Theme</ThemeToggle>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
